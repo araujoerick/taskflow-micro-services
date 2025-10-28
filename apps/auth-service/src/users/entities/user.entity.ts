95cd4aeeb473d85338d4da57345e9b11b6eeddd0
Erick Araújo <araujoerick.dev@gmail.com>
2025-10-28 00:29:41 -0300

    feat: add user entity and database migration

---
+++ apps/auth-service/src/users/entities/user.entity.ts
@@ -0,0 +1,31 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ length: 255 })
+  name: string;
+
+  @Column({ unique: true, length: 255 })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ nullable: true })
+  refreshToken?: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
