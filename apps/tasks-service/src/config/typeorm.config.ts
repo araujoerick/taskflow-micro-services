5417a1b99d464748c260f8cb5eb2683b15a4bda0
Erick Araújo <araujoerick.dev@gmail.com>
2025-10-28 22:51:26 -0300

    chore: add environment configuration for tasks-service

---
+++ apps/tasks-service/src/config/typeorm.config.ts
@@ -0,0 +1,22 @@
+import { ConfigService } from '@nestjs/config';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { config } from 'dotenv';
+
+config();
+
+const configService = new ConfigService();
+
+export const typeOrmConfig: DataSourceOptions = {
+  type: 'postgres',
+  host: configService.get<string>('DB_HOST'),
+  port: configService.get<number>('DB_PORT'),
+  username: configService.get<string>('DB_USERNAME'),
+  password: configService.get<string>('DB_PASSWORD'),
+  database: configService.get<string>('DB_DATABASE'),
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/../migrations/*{.ts,.js}'],
+  synchronize: false,
+  logging: configService.get<string>('NODE_ENV') === 'development',
+};
+
+export default new DataSource(typeOrmConfig);
