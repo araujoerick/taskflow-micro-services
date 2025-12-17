7d721da3a16bb955f75ebaa0e2197d44289c32d6
Erick Araújo <araujoerick.dev@gmail.com>
2025-12-17 00:55:34 -0300

    feat: add environment variables validation schema

---
+++ apps/tasks-service/src/config/env.validation.ts
@@ -0,0 +1,70 @@
+import { plainToInstance } from 'class-transformer';
+import {
+  IsEnum,
+  IsNumber,
+  IsString,
+  validateSync,
+  IsUrl,
+  Min,
+  Max,
+} from 'class-validator';
+
+enum Environment {
+  Development = 'development',
+  Production = 'production',
+  Test = 'test',
+}
+
+export class EnvironmentVariables {
+  @IsEnum(Environment)
+  NODE_ENV: Environment;
+
+  @IsNumber()
+  @Min(1)
+  @Max(65535)
+  PORT: number;
+
+  // Database
+  @IsString()
+  DB_HOST: string;
+
+  @IsNumber()
+  @Min(1)
+  @Max(65535)
+  DB_PORT: number;
+
+  @IsString()
+  DB_USERNAME: string;
+
+  @IsString()
+  DB_PASSWORD: string;
+
+  @IsString()
+  DB_DATABASE: string;
+
+  // JWT
+  @IsString()
+  JWT_SECRET: string;
+
+  // RabbitMQ
+  @IsUrl({ require_tld: false })
+  RABBITMQ_URL: string;
+
+  @IsString()
+  RABBITMQ_QUEUE: string;
+}
+
+export function validate(config: Record<string, unknown>) {
+  const validatedConfig = plainToInstance(EnvironmentVariables, config, {
+    enableImplicitConversion: true,
+  });
+
+  const errors = validateSync(validatedConfig, {
+    skipMissingProperties: false,
+  });
+
+  if (errors.length > 0) {
+    throw new Error(errors.toString());
+  }
+  return validatedConfig;
+}
