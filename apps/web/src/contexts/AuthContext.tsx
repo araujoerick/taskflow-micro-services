691dd369e7581a38b8d7a3e3832b4132c6bfc7ec
Erick Araújo <araujoerick.dev@gmail.com>
2025-12-27 11:09:02 -0300

    chore: disable react-refresh rule to AuthContext

--- apps/web/src/contexts/AuthContext.tsx
+++ apps/web/src/contexts/AuthContext.tsx
@@ -82,7 +82,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
     </AuthContext.Provider>
   );
 }
-
+// eslint-disable-next-line react-refresh/only-export-components
 export function useAuth() {
   const context = useContext(AuthContext);
   if (context === undefined) {
